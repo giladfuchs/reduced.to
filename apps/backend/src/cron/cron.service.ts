e6e07f75ce39ac1b502f76278a75635ba5db1225
giladfuchs <igilfu@gmail.com>
2024-02-13 12:23:11 +0200

    add cron job that clen all link that expirationTime has passed

--- apps/backend/src/cron/cron.service.ts
+++ apps/backend/src/cron/cron.service.ts
@@ -6,21 +6,20 @@ import { LinksService } from '../core/links/links.service';
 import { calculateSkip } from '../shared/utils';
 import { IFindAllOptions } from '../core/entity.service';
 import { Link } from '@reduced.to/prisma';
-import {AppLoggerSerivce} from "@reduced.to/logger";
+import { AppLoggerSerivce } from '@reduced.to/logger';
 
 @Injectable()
 export class CronService {
-  constructor(private readonly linksService: LinksService,
-              private readonly logger: AppLoggerSerivce) {}
+  constructor(private readonly linksService: LinksService, private readonly logger: AppLoggerSerivce) {}
 
   @Cron(CronExpression.EVERY_DAY_AT_MIDNIGHT)
-   async dailyCleanLink() {
+  async dailyCleanLink() {
     this.logger.log(`start to run cron job to clean link`);
 
     const links = await this.linksService.findAll({} as IFindAllOptions);
     const currentTime = new Date();
     //only after 30 days the link is expired
-    currentTime.setDate(currentTime.getDate() -30);
+    currentTime.setDate(currentTime.getDate() - 30);
 
     // Filter the links whose expirationTime has passed
     const validLinks: Link[] = links.data.filter((link) => link.expirationTime && link.expirationTime < currentTime) as Link[];
@@ -29,7 +28,6 @@ export class CronService {
       await this.linksService.delete(link.id);
     }
     this.logger.log(`finish to run cron job to clean link`);
-
   }
 
   // @Cron('*/20 * * * * *')
